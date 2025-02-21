699b960c034e492cf333a08c7991e23b73e45bd4
chiagoziepio <nduagubapio@gmail.com>
2025-02-21 12:18:41 +0100

    produ 4th retry

--- next-auth-d.ts
+++ next-auth-d.ts
@@ -1,11 +1,19 @@
-import { type DefaultSession } from "next-auth";
-
-export type ExtendedUser = DefaultSession["user"] & {
-  isOauth: boolean;
-};
+import { DefaultSession } from "next-auth";
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+import type { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-  interface session {
-    user: ExtendedUser;
+  interface Session {
+    user: {
+      /** Indicates if the sign-in was via OAuth */
+      isOauth: boolean;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    /** Indicates if the sign-in was via OAuth */
+    isOauth: boolean;
   }
 }
